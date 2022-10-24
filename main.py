0be2a93d7086630a0c91a164b1d60e396e02e512
callmeshekhar <64550115+callmeshekhar@users.noreply.github.com>
2022-10-23 22:53:21 -0500

    pushing final files to GitHub repository

--- main.py
+++ main.py
@@ -119,12 +119,15 @@ def go(config: DictConfig):
 
 
         if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                uri=f"{config['main']['components_repository']}/test_regression_model",
+               #entry_point='main',
+		version='main',
+                parameters={
+                    'mlflow_model': 'random_forest_export:prod',
+                    'test_dataset': 'test_data.csv:latest',
+                },
+            )
 
 
 if __name__ == "__main__":
